f9c97545f1088a7887703a6ee9624f737f15df67
mchave10 <mchave10@hotmail.com>
2018-01-24 21:50:12 -0500

    testing my repository

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="robots" content="noindex, nofollow">  
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Awesome Trilogy Lander!</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="css/index.css" rel="stylesheet"> 
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <footer>
+
+        </footer>
+        <script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>  
+    </body>
+</html>
